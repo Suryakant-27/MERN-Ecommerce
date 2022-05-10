c3d60d0b26c8e877689570613e750d0e867cdf6e
Suryakant <suryakantb33@gmail.com>
2022-05-10 21:19:24 +0530

    Sign in Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import Nav from 'react-bootstrap/esm/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -41,8 +42,9 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
-              <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
         </main>
